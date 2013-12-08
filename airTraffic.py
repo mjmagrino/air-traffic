c6fc910f1e37f7358ac9a726eb90b28feb8ed3ce
matt <mjmagrino@gmail.com>
2013-12-07 22:08:35 -0500

    correctly references all nodes with anti-parallel edges and outputs their edges and edge weights

--- airTraffic.py
+++ airTraffic.py
@@ -15,13 +15,6 @@ def main(argv):
     file2 = argv[2]
     file3 = argv[3]
 
-    """  
-    network = pgv.AGraph(file1,directed=True)
-    traffic = pgv.AGraph(file3, directed=True)
-    network = nx.from_agraph(network)
-    traffic = nx.from_agraph(traffic)
-    """
-
     network = nx.read_dot(file1) 
     traffic = nx.read_dot(file3)
     
@@ -33,24 +26,33 @@ def main(argv):
         nodeCaps[str(splitLine[0])]= int(splitLine[1])
 
     #while (network.number_of_edges())>(int(network.number_of_nodes())/2):
-    backedgeCheck=[]
+
     for node1 in network:
-        for node2 in network:
-            if node1 != node2:
-                backedgeCheck.append((node1,node2))
-                if (node1,node2) and (node2,node1) in backedgeCheck:
-                    #do anti-parallel edge removal by adding intermediate node
-                    print "there is an anti-parallel edge!"
-                    
-                else:
-                    n = network.number_of_edges(node1, node2)
-                    print(n)
-                
-                
-    print backedgeCheck
-    #print ("network nodes: " + str(network.nodes()))
-    #print "~~~~~~~~~~~~~~"
-    #print ("network edges: " +  str(network.edges()))
+	for node2 in network:
+	    if node1 != node2:
+	    
+		if(node1 in network.neighbors(node2) and (node2 in network.neighbors(node1))): 
+		
+		    print "~~~~~~~~~~~~~~~~~~~"
+		    print(node1+"->"+node2+" weight: " + str(network[node1][node2][0]['label']))    
+		
+		elif (node2 in network.neighbors(node1)and (node1 in network.neighbors(node2))):
+	
+		    print "~~~~~~~~~~~~~~~~~~~"
+		    print(node2+"->"+node1+" weight: " + str(network[node2][node1][0]['label']))
+
+		else:
+		    n = network.number_of_edges(node1, node2)
+		    #print(n)              
+		     
+		
+	    #do anti-parallel edge removal by adding intermediate node
+    
+    #print(network.nodes())
+    print("~~~~~~~~~~~~~~~~~~~")
+    #print(network.edges())
+
+    #print(network)
     #print(nodeCaps)
     #print(traffic)
 
